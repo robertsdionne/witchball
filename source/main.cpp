11b2dcad5bbc0c5604ed597e66cf53e0c5fa5855
Robert Scott Dionne <robertsdionne@gmail.com>
2013-03-08 20:39:32 -0500

    Add Box2D. Rename 'src' to 'source'. Fix main(). Add Box2D header search paths.

--- source/main.cpp
+++ source/main.cpp
@@ -5,7 +5,7 @@
 constexpr int kWidth = 1024;
 constexpr int kHeight = 768;
 
-int main( ){
+int main(const int argument_count, const char *arguments[]) {
   ofAppGlutWindow window;
   ofSetupOpenGL(&window, kWidth, kHeight, OF_WINDOW);
   ofRunApp(new WitchBall());
