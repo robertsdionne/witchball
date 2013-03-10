e772ce8cdd7477c21c2b3a3070f0d9267db5e161
Robert Scott Dionne <robertsdionne@gmail.com>
2013-03-09 22:30:37 -0500

    Comment the utilities functions.

--- source/utilities.h
+++ source/utilities.h
@@ -5,12 +5,24 @@
 
 #include "ofMain.h"
 
+/**
+ * Creates a b2Vec2 from an ofVec2f.
+ */
 b2Vec2 Box2dVector(ofVec2f openframeworks_vector);
 
+/**
+ * Linearly interpolates a b2Vec2 "from" to a b2Vec2 "to" by the amount "t".
+ */
 b2Vec2 Lerp(b2Vec2 from, b2Vec2 to, float t);
 
+/**
+ * Linearly interpolates an ofVec2f "from" to an ofVec2f "to" by the amount "t".
+ */
 ofVec2f Lerp(ofVec2f from, ofVec2f to, float t);
 
+/**
+ * Creates an ofVec2f from a b2Vec2.
+ */
 ofVec2f OpenFrameworksVector(b2Vec2 box2d_vector);
 
 #endif  // WITCHBALL_UTILITIES_H_
